ef177740fd1ca0c75b42932453dc79c0946acdaa
BHAVY1503 <bhavypatel1503@gmail.com>
2025-11-10 14:08:02 +0530

    update pyment method

---
+++ src/models/PaymentModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  organizerId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'organizer'
+
+  },
+  eventId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'event',
+    required: true
+  },
+  orderId: {
+    type: String,
+    required: true
+  },
+  paymentId: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'completed', 'failed'],
+    default: 'pending'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('payment', paymentSchema);
